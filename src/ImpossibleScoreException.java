9b382e9aa5f5971a8acea93bf7da8954504c1efe
Alex Gonzalez <galex2003@icloud.com>
2023-10-29 23:31:14 -0400

    Got rid of unneccessary player class

--- src/ImpossibleScoreException.java
+++ src/ImpossibleScoreException.java
@@ -1,7 +1,7 @@
 public class ImpossibleScoreException extends Throwable {
 
     public ImpossibleScoreException(int num) {
-        super(num + " is an invalid darts score.");
+        super(num + " is an invalid darts score.\n");
     }
 
 
