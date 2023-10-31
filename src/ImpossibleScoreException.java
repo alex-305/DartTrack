ddc1ebb350a8d89b9efffa99d49e72a0dd4f1c3c
Alex Gonzalez <galex2003@icloud.com>
2023-10-31 13:25:28 -0400

    Updated exception message to display the accurate number.

--- src/ImpossibleScoreException.java
+++ src/ImpossibleScoreException.java
@@ -1,7 +1,7 @@
 public class ImpossibleScoreException extends Throwable {
 
     public ImpossibleScoreException(int num, int mult) {
-        super(num*mult + " is an invalid darts score.\n");
+        super(num + " * " + mult + " is an invalid darts score.\n");
     }
 
 
